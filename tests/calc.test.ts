b135dc86b789f494384416cc76c088b39965e9ac
Bhadresh Desai <16066580+bhadreshdesai@users.noreply.github.com>
2020-11-26 11:28:42 +0000

    Update test to increase coverage

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -5,4 +5,8 @@ describe("Calculator Test Suite", () => {
     const calc = new Calculator();
     expect(calc.add(2, 3)).toBe(5);
   });
+  test("Test subtract", () => {
+    const calc = new Calculator();
+    expect(calc.subtract(5, 3)).toBe(2);
+  });
 });
